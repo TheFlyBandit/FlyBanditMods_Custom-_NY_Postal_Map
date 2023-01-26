2670f1c947af0d3faacecb7463c79cfafd6c6be3
TheFlyBandit <102429379+TheFlyBandit@users.noreply.github.com>
2023-01-26 14:31:50 -0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'on'
+
+author 'TheFlyBandit'
+description 'Postal map based on New York City'
+discord 'https://discord.gg/MH9BADQuZy'
+
+this_is_a_map 'yes'
+
+client_script "client.lua"
